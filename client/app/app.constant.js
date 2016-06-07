7846bf6b611d96d80a8dc3c4c5e02115c6d9860e
Josue Mancilla <josue@autoboxcorp.com>
2016-06-07 14:25:27 +0000

    Improving html styles
     - changes in paginations links
     - form style
     - form style
     - again form styles correction
     - changes in forms
     - after mergin developt

--- client/app/app.constant.js
+++ client/app/app.constant.js
@@ -3,7 +3,7 @@
 
 angular.module('fakiyaMainApp.constants', [])
 
-.constant('appConfig', {userRoles:['guest','user','admin'],apiUri:'http://u19212546.onlinehome-server.com:8080/api'})
+.constant('appConfig', {userRoles:['guest','user','admin'],apiUri:'http://localhost:9000/api'})
 
 ;
 })(angular);
