a753e119ccdadd773cb173b113c8fa297d043bd3
Enrique <enrique@ortuno.net>
2016-06-13 11:49:53 -0400

    Fixing production errors

--- server/common/middlewares/corsenabler.js
+++ server/common/middlewares/corsenabler.js
@@ -2,7 +2,7 @@
 
 import cors from 'cors';
 
-let whitelist = ['http://www.dev-autoboxcorp.com:8080', 'http://example2.com'];
+let whitelist = ['http://www.dev-autoboxcorp.com:8080', 'http://www.dev-autoboxcorp.com'];
 let corsOptions = {
     origin: function (origin, callback) {
         console.log('Evaluating origin -> ' + origin);
