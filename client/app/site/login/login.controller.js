3723430cb00604f64ed1fdb5f0001bbcd867c259
Josue Mancilla <josue@autoboxcorp.com>
2016-07-06 19:14:36 +0000

    Including a new directive in order to limit the maximum characters
    validation on inputs, and control characters

--- client/app/site/login/login.controller.js
+++ client/app/site/login/login.controller.js
@@ -1,28 +1,29 @@
 'use strict';
 
-(function() {
+(function () {
 
-class LoginController {
+  class LoginController {
 
-  constructor($state) {
-    this.state=$state;
-    this.username='';
-    this.password='';   
-    this.message={show:false};
-  }
-
-  autentichate(){
-    if(this.username==='autobox' && this.password==='autobox'){
-      this.state.go('ap.al.skills');
-
-    }else{
-      this.message={show:true,text:'Invalid username and/or password. Please try again',type:'danger',expires:4000};
+    constructor($state) {
+      this.state = $state;
+      this.username = '';
+      this.password = '';
+      this.message = { show: false };
     }
+
+    autentichate() {
+      if (this.username === 'autobox' && this.password === 'autobox') {
+        this.state.go('ap.al.skills');
+      } else {
+        this.message = { show: true, text: 'Invalid username and/or password. Please try again', type: 'danger', expires: 3000 };
+        this.password = '';
+      }
+    }
+
   }
-    
-}
-  
-LoginController.$inject = ['$state'];
+
+  LoginController.$inject = ['$state'];
+
 
 
 angular.module('fakiyaMainApp')
@@ -30,5 +31,4 @@ angular.module('fakiyaMainApp')
     templateUrl: 'app/site/login/login.html',
     controller: LoginController
   });
-
 })();
