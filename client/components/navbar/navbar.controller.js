5c5044c7666649ae49509db14984ba338ddcd364
jujomago <jujomago@gmail.com>
2016-07-08 12:17:07 -0400

    UI fixes

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -1,42 +1,33 @@
 'use strict';
 
 class NavbarController {
-  //start-non-standard
-  menu = [{
-    'title': 'Dashboard',
-    'state': 'main',
-     'link':'/underconstruction' 
-  },
-  {
-    'title': 'My Apps',
-    'state': 'skills.list',
-     'link':'/underconstruction'
-  },
-    {
-    'title': 'Reports',
-    'state': 'users.list',
-     'link':'/underconstruction'
-    
-  }
-  ];
 
-  
-  isCollapsed = true;
-  //end-non-standard
+  constructor() {
 
-  constructor($sessionStorage) {
-console.log($sessionStorage);
+    this.isCollapsed = true;
+    this.userOptionsCollapsed = true;
 
 
-    this.hideNavBar=false;
-    
-    if(!$sessionStorage.logged){
-      this.hideNavBar=true;
-    }
+    this.menu = [{
+      'title': 'Dashboard',
+      'state': 'main',
+      'link': '/underconstruction'
+    },
+      {
+        'title': 'My Apps',
+        'state': 'skills.list',
+        'link': '/underconstruction'
+      },
+      {
+        'title': 'Reports',
+        'state': 'users.list',
+        'link': '/underconstruction'
+      }
+    ];
+
   }
 }
 
-NavbarController.$inject=['$sessionStorage'];
 
 angular.module('fakiyaMainApp')
   .controller('NavbarController', NavbarController);
