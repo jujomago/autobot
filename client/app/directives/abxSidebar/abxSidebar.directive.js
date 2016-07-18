30db85220f08224ac3b5131f0d08178c33dc473a
Daniel Candia <daniel.c@autoboxcorp.com>
2016-07-18 23:18:34 +0000

    Including abx-sidebar directive
     - bugs 188 & 197

---
+++ client/app/directives/abxSidebar/abxSidebar.directive.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('fakiyaMainApp')
+  .directive('abxSidebar', ['$location', function($location) {
+
+  function link(scope) {
+    scope.checkActive = function(link){
+      		return ($location.path().indexOf(link) >= 0) ? 'active' : '';
+    };
+  }
+
+  return {
+    templateUrl: 'app/directives/abxSidebar/abxSidebar.html',
+    restrict: 'EA',
+    link: link
+  };
+}]);
