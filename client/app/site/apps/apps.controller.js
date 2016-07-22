29106c3ed65bc25cd3b2c8ce93bf2469347b49e8
Enrique <enrique@ortuno.net>
2016-07-22 18:38:32 -0400

    merge integration

---
+++ client/app/site/apps/apps.controller.js
@@ -0,0 +1,32 @@
+'use strict';
+(function(){
+	class AppsComponent {
+	constructor($state, AppsService) {
+		this.partners = [];
+		this.message = {show: false};
+		this.AppsService = AppsService;
+	}
+	$onInit(){
+		this.getApps();
+	}
+	getApps(){
+		return this.AppsService.getApps()
+		.then(response => {
+			this.partners=response.data;
+			return response;
+		})
+		.catch(error => {
+			let theMsg= error.error; 
+            this.message={ show: true, type: 'danger', text: theMsg };
+            return error;
+		});
+	}
+}
+	AppsComponent.$inject = ['$state', 'AppsService'];
+	angular.module('fakiyaMainApp')
+	  .component('apps', {
+	    templateUrl: 'app/site/apps/apps.html',
+	    controller: AppsComponent
+	  });
+
+})();
