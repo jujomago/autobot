e9e62fa32d8c784db3f7062facfd4c14202a292d
Enrique <enrique@ortuno.net>
2016-07-22 12:39:11 -0400

    page without service

--- client/app/site/apps/apps.controller.js
+++ client/app/site/apps/apps.controller.js
@@ -4,7 +4,7 @@
 class AppsComponent {
   constructor() {
     this.message = 'Hello';
-    this.partners = [{partnerName: 'Five9', partnerId: 'f9', apps: [{appName: 'al', appFullName: 'Admin Lite', description: 'This is the description', isInstalled: true}]},{partnerName: 'SalesForce', partnerId: 'sf', apps: [{appName: 'xy', appFullName: 'XYZ App', description: 'This is the description', isInstalled: false},{appName: 'ab', appFullName: 'ABC admin', description: 'This is the description', isInstalled: false}]}]
+    this.partners = [{partnerName: 'Five9', partnerId: 'f9', apps: [{appName: 'al', appFullName: 'Admin Lite', description: 'This is the description', isInstalled: true},{appName: 'al', appFullName: 'Admin Lite', description: 'This is the description', isInstalled: true},{appName: 'al', appFullName: 'Admin Lite', description: 'This is the description', isInstalled: true},{appName: 'al', appFullName: 'Admin Lite', description: 'This is the description', isInstalled: true},{appName: 'al', appFullName: 'Admin Lite', description: 'This is the description', isInstalled: true}]},{partnerName: 'SalesForce', partnerId: 'sf', apps: [{appName: 'sf', appFullName: 'XYZ App', description: 'This is the description', isInstalled: false}]}]
   }
 }
 
