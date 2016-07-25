6dfeac304bb8ee00f32c0724553b6ef05f22a6a9
Enrique <enrique@ortuno.net>
2016-07-25 17:32:31 -0400

    add list to modal

--- client/app/features/al/lists/create/create.controller.js
+++ client/app/features/al/lists/create/create.controller.js
@@ -1,12 +1,43 @@
 'use strict';
 (function(){
 	let _ListService, _$state;
+    function _getFormatedMessage(message)
+    {
+        switch(message)
+        {
+            case ORIGINAL_CAMPAIGN_MESSAGE:
+                return CUSTOM_CAMPAIGN_MESSAGE;
+            case ORIGINAL_SYSTEM_MESSAGE:
+                return CUSTOM_SYSTEM_MESSAGE;
+        }
+        return message;
+    }
+    function _getErrorMessage(xmlMessage)
+    {
+        let firstStep=xmlMessage.split('<faultstring>');
+        if(firstStep.length<2){
+            return xmlMessage;
+        }
+        let secondStep=firstStep[1].split('</faultstring>');
+        if(secondStep.length>0){
+            return secondStep[0];
+        }
+        return xmlMessage;
+    }
 	class CreateComponent {
 	  constructor($state, ListsService) {
-	    this.message = { show: false }; 
+	    this.message = {text: ''}; 
 	    this.SubmitText = 'Save';
 	    _ListService = ListsService;
 	    _$state = $state;
+	  }
+	  $onInit(){
+	  	this.instance = this.parent.modalInstance;
+	  }
+	  close(){
+	  	if(this.instance){
+	  		this.instance.dismiss('cancel');
+	  	}
 	  }
 	  save(){
 	  	this.SubmitText = 'Saving...';
@@ -18,11 +49,12 @@
 	                    text: 'List Created SuccessFully',
 	                    expires: 3000
 	                };
-	                _$state.go('ap.al.lists', { message: this.message });   
+	                this.close();
+	                _$state.go('ap.al.lists', { message: this.message }, {reload: true});   
 	            }
 	            else{
 		            this.SubmitText='Save';
-		            let theMsg= response.errorMessage; 
+		            let theMsg= _getErrorMessage(response.errorMessage); 
 		            this.message={ show: true, type: 'danger', text: theMsg, expires: 5000 };
 	            }
 	            return response;
@@ -33,7 +65,10 @@
 	angular.module('fakiyaMainApp')
 	  .component('al.lists.create', {
 	    templateUrl: 'app/features/al/lists/create/create.html',
-	    controller: CreateComponent
+	    controller: CreateComponent,
+	    require: {
+            parent : '^al.lists.list'
+        }
 	  });
 
 })();
