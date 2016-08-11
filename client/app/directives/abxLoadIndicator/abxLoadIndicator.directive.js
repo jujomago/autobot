84ae27aa1b28873e0bbfeac1ef4579b5c2fb24f1
Josue Mancilla <josue@autoboxcorp.com>
2016-08-11 12:41:20 +0000

    Fixing load indicator directive issues
     - fixed bugs
    
    Related work items: #776, #778, #952

--- client/app/directives/abxLoadIndicator/abxLoadIndicator.directive.js
+++ client/app/directives/abxLoadIndicator/abxLoadIndicator.directive.js
@@ -1,11 +1,38 @@
 'use strict';
 
 angular.module('fakiyaMainApp')
-  .directive('abxLoadIndicator', function () {
+  .directive('abxLoadIndicator', function ($window) {
     return {
       templateUrl: 'app/directives/abxLoadIndicator/abxLoadIndicator.html',
       restrict: 'E',     
       link: function (scope, element, attrs) {
+
+
+        let loaderIndicator= element.find('.loader_indicator');
+        let rightSide=angular.element('.rightside');
+        let mainContainer=$('#main-container');
+
+        function centerMessage() {  
+
+          let widthRightSide=rightSide.innerWidth();
+          let rightOffset=Math.trunc((widthRightSide-200)/2);
+          loaderIndicator.css('right',rightOffset);
+
+
+          let heightViewPort=mainContainer.innerHeight();
+          let topOffset=Math.trunc((heightViewPort/2)-50);
+          loaderIndicator.css('top',topOffset);
+
+       }
+
+        angular.element($window).on('resize', function() {
+            scope.$apply(function() {
+                centerMessage();
+            });
+        });
+
+        centerMessage();
+
 
         if(attrs.text){
            element.find('p').html(attrs.text);
