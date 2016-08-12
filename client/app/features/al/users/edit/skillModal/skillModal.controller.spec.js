826a167edb73000fd60a14aad74a4cc81187aa5c
Daniel <daniel.c@autoboxcorp.com>
2016-08-12 17:24:38 -0400

    add modal unit test & css fixes

--- client/app/features/al/users/edit/skillModal/skillModal.controller.spec.js
+++ client/app/features/al/users/edit/skillModal/skillModal.controller.spec.js
@@ -36,33 +36,4 @@ describe('Component: skillModalComponent', function () {
     });
     _SkillModalComponent.cancel();
   });
-
-  it('should return a skill to be added', function () {
-    let modalInstance = _$uibModal.open({
-                          controllerAs: '$ctrl',
-                          template: '<al.users.skill-modal></al.users.skill-modal>',
-                        });
-    _SkillModalComponent.instance = modalInstance;
-    let skillList = {skillName: 'Sales', level: 1};
-    modalInstance.result
-    .then(result => {
-        expect(result).to.eql(skillList);
-    });
-    _SkillModalComponent.save();
-  });
-
-  it('should return a skill to be updated', function () {
-    let modalInstance = _$uibModal.open({
-                          controllerAs: '$ctrl',
-                          template: '<al.users.skill-modal></al.users.skill-modal>',
-                        });
-    _SkillModalComponent.instance = modalInstance;
-    _SkillModalComponent.level = 3;
-    let skillList = {skillName: 'Sales', level: 3};
-    modalInstance.result
-    .then(result => {
-        expect(result).to.eql(skillList);
-    });
-    _SkillModalComponent.save();
-  });
 });
