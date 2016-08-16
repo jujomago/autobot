25e91f4b48b887e145c93bae728e391aa7fa51aa
Nelson Araoz <nelson@autoboxcorp.com>
2016-08-16 14:55:09 +0000

    Solving provider error in production
     - fixing error

--- client/app/factories/AlertMessage/AlertMessage.service.js
+++ client/app/factories/AlertMessage/AlertMessage.service.js
@@ -7,12 +7,12 @@ angular.module('fakiyaMainApp')
         animation: false,
         templateUrl: 'app/factories/AlertMessage/AlertMessage.html',
         size: 'hs',
-        controller: function ($scope, $uibModalInstance, content) {
+        controller: ['$scope', '$uibModalInstance', 'content', function ($scope, $uibModalInstance, content) {
           $scope.content = content;
           $scope.close = function(){
             $uibModalInstance.dismiss('cancel');
           };
-        },
+        }],
         resolve: {
           content: function () {
             return content;
