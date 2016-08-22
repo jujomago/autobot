d33bf3bb7b630cd716923f030fc8a51b70250dd8
Daniel Candia <daniel.c@autoboxcorp.com>
2016-08-22 22:31:01 +0000

    Apps-submenu to develop
     - submenu
    
    Related work items: #114

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -1,11 +1,25 @@
 'use strict';
-
+let _lodash;
+let _appsService;
+let _$filter, _$parse;
 class NavbarController {
 
-  constructor() {
+  constructor($filter, $parse, lodash, AppsService) {
 
     this.isCollapsed = true;
     this.userOptionsCollapsed = true;
+    this.myAppsCollapsed = true;
+    this.quantity = 4;
+    this.search = {app: {appFullName: ''}};
+    this.appsLoaded = false;
+    _lodash = lodash;
+    _$filter = $filter;
+    _$parse = $parse;
+    _appsService = AppsService;
+    this.fullMenu = false;
+    this.minMenu = false;
+    this.partners = [];
+    this.getter = 'partner.partnerFullName';
 
 
     this.menu = [{
@@ -16,7 +30,7 @@ class NavbarController {
       {
         'title': 'My Apps',
         'state': 'skills.list',
-        'link': '/underconstruction'
+        'link': '/underconstruction',
       },
       {
         'title': 'Reports',
@@ -24,10 +38,76 @@ class NavbarController {
         'link': '/underconstruction'
       }
     ];
- 
+
+    this.myAppsFromService = [];
+
+    this.myAppsSearch = {};
+
+    this.total = 0;
+
+    this.myApps = [];
+
+    this.newApps = [];
+
  }
+
+ $onInit(){
+  this.getInstalled();
+  this.getNewest();
+ }
+ 
+  getInstalled(){
+    return _appsService.getInstalled().then(response => {
+      this.myAppsFromService = response.data;
+      this.myAppsSearch = this.myAppsFromService;
+      this.myAppsSearch = this.groupBy(this.myAppsSearch);
+      this.fullMenu = (this.myAppsFromService.length > 4) ? true : false;
+      this.total = this.myAppsFromService.length;
+      return response;
+    })
+    .catch(error => {
+      let theMsg= error.errorMessage; 
+      this.message={ show: true, type: 'danger', text: theMsg };
+      return error;
+    });
+  }
+
+  getNewest(){
+    return _appsService.getNewest().then(response => {
+      this.newApps = response.data;
+      return response;
+    })
+    .catch(error => {
+      let theMsg= error.errorMessage; 
+      this.message={ show: true, type: 'danger', text: theMsg };
+      return error;
+    });
+  }
+
+  filteringBySearch(){
+    
+    this.myAppsSearch = _$filter('filter')(this.myAppsFromService, this.search.app.appFullName);
+    
+    this.myAppsSearch = this.groupBy(this.myAppsSearch);
+
+    if(this.search.app.appFullName){
+      this.total = Object.keys(this.myAppsSearch).length;
+      return true;
+    }else{
+      this.total = this.myAppsFromService.length;
+      return false;
+    }
+  }
+
+  groupBy(list){
+    let getter = _$parse(this.getter);
+    return _lodash.groupBy(list, function(item) {
+        return getter(item);
+    });  
+  }
+
 }
 
-
+NavbarController.$inject=['$filter', '$parse' ,'lodash', 'AppsService'];
 angular.module('fakiyaMainApp')
   .controller('NavbarController', NavbarController);
