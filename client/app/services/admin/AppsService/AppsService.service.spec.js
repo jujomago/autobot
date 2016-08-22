0521d8b3fb319f3139a684ade35ffb89d93b715a
Daniel Candia <daniel.c@autoboxcorp.com>
2016-08-22 22:32:53 +0000

    Submenu implementation
     - submenu
    
    Related work items: #114

--- client/app/services/admin/AppsService/AppsService.service.spec.js
+++ client/app/services/admin/AppsService/AppsService.service.spec.js
@@ -66,4 +66,94 @@ describe('Service: AppsService', function () {
     _$httpBackend.flush();
   });
 
+  it('should return submenu installed apps', function () {
+    let installedApps = [
+    {
+      'app':{
+        'appName': 'AL', 
+        'appFullName': 'Admin Console',
+        'description': 'My app 1 description'
+      },
+      'partner':{
+        'partnerName': 'f9',
+        'partnerFullName': 'Five9'
+      }  
+    },
+    {
+      'app':{
+        'appName': 'myapp2', 
+        'appFullName': 'My App2',
+        'description': 'My app 2 description'
+      },
+      'partner':{
+        'partnerName': 'f9',
+        'partnerFullName': 'Five9'
+      }
+    }];
+    _$httpBackend.whenGET('/assets/admin/json/installed.json').respond(installedApps);
+    _AppsService.getInstalled().then(apps => {
+        expect(null).to.not.equal(apps);
+        expect(undefined).to.not.equal(apps);
+        expect(apps.data.length).to.equal(2);
+        expect(apps.data[0].partner.partnerFullName).to.equal('Five9');
+    });
+    _$httpBackend.flush();
+  });
+  it('should return submenu error', function () {
+    _$httpBackend.whenGET('/assets/admin/json/installed.json').respond(500, 'Internal Server Error');
+    _AppsService.getInstalled().catch(error => {
+        expect(null).to.not.equal(error);
+        expect(undefined).to.not.equal(error);
+        expect(error.statusCode).to.equal(500);
+        expect(error.errorMessage).to.equal('Internal Server Error');
+    });
+    _$httpBackend.flush();
+  });
+
+  it('should return submenu new apps', function () {
+    let newApps = [
+      {
+        'app':{
+          'appName': 'myapp4', 
+          'appFullName': 'My App4',
+          'description': 'My app 4 description'
+        },
+        'partner':{
+          'partnerName': 'f9',
+          'partnerFullName': 'Five9'
+        }
+      },
+      {
+        'app':{
+          'appName': 'myapp5', 
+          'appFullName': 'My App5',
+          'description': 'My app 5 description'
+        },
+        'partner':{
+          'partnerName': 'sf',
+          'partnerFullName': 'SalesForce'
+        }
+      }
+    ];
+    _$httpBackend.whenGET('/assets/admin/json/newapps.json').respond(newApps);
+    _AppsService.getNewest().then(apps => {
+        expect(null).to.not.equal(apps);
+        expect(undefined).to.not.equal(apps);
+        expect(apps.data.length).to.equal(2);
+        expect(apps.data[1].partner.partnerFullName).to.equal('SalesForce');
+    });
+    _$httpBackend.flush();
+  });
+
+  it('should return submenu new apps error', function () {
+    _$httpBackend.whenGET('/assets/admin/json/newapps.json').respond(500, 'Internal Server Error');
+    _AppsService.getNewest().catch(error => {
+        expect(null).to.not.equal(error);
+        expect(undefined).to.not.equal(error);
+        expect(error.statusCode).to.equal(500);
+        expect(error.errorMessage).to.equal('Internal Server Error');
+    });
+    _$httpBackend.flush();
+  });
+
 });
--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -1,11 +1,12 @@
 'use strict';
 
-
 let _$location,_auth;
+let _lodash;
+let _appsService;
+let _$filter, _$parse;
 class NavbarController {
 
-  constructor($location,AuthService) {
-
+  constructor($filter, $parse, $location, lodash, AuthService, AppsService) {
 
     this.isCollapsed = true;
  
@@ -14,6 +15,19 @@ class NavbarController {
    
     _$location=$location;
     _auth=AuthService;
+    this.myAppsCollapsed = true;
+    this.quantity = 4;
+    this.search = {app: {appFullName: ''}};
+    this.appsLoaded = false;
+    _lodash = lodash;
+    _$filter = $filter;
+    _$parse = $parse;
+    _appsService = AppsService;
+    this.fullMenu = false;
+    this.minMenu = false;
+    this.partners = [];
+    this.getter = 'partner.partnerFullName';
+
     this.menu = [{
       'title': 'Dashboard',
       'state': 'main',
@@ -22,7 +36,7 @@ class NavbarController {
       {
         'title': 'My Apps',
         'state': 'skills.list',
-        'link': '/underconstruction'
+        'link': '/underconstruction',
       },
       {
         'title': 'Reports',
@@ -30,13 +44,81 @@ class NavbarController {
         'link': '/underconstruction'
       }
     ];
-  }
-  logout(){
+
+    this.myAppsFromService = [];
+
+    this.myAppsSearch = {};
+
+    this.total = 0;
+
+    this.myApps = [];
+
+    this.newApps = [];
+ }
+
+ logout(){
     _auth.logout();
     _$location.path('/login');  
   }
+
+  $onInit(){
+    this.getInstalled();
+    this.getNewest();
+  }
+ 
+  getInstalled(){
+    return _appsService.getInstalled().then(response => {
+      this.myAppsFromService = response.data;
+      this.myAppsSearch = this.myAppsFromService;
+      this.myAppsSearch = this.groupBy(this.myAppsSearch);
+      this.fullMenu = (this.myAppsFromService.length > 4) ? true : false;
+      this.total = this.myAppsFromService.length;
+      return response;
+    })
+    .catch(error => {
+      let theMsg= error.errorMessage; 
+      this.message={ show: true, type: 'danger', text: theMsg };
+      return error;
+    });
+  }
+
+  getNewest(){
+    return _appsService.getNewest().then(response => {
+      this.newApps = response.data;
+      return response;
+    })
+    .catch(error => {
+      let theMsg= error.errorMessage; 
+      this.message={ show: true, type: 'danger', text: theMsg };
+      return error;
+    });
+  }
+
+  filteringBySearch(){
+    
+    this.myAppsSearch = _$filter('filter')(this.myAppsFromService, this.search.app.appFullName);
+    
+    this.myAppsSearch = this.groupBy(this.myAppsSearch);
+
+    if(this.search.app.appFullName){
+      this.total = Object.keys(this.myAppsSearch).length;
+      return true;
+    }else{
+      this.total = this.myAppsFromService.length;
+      return false;
+    }
+  }
+
+  groupBy(list){
+    let getter = _$parse(this.getter);
+    return _lodash.groupBy(list, function(item) {
+        return getter(item);
+    });  
+  }
+
 }
 
-NavbarController.$inject=['$location','AuthService'];
+NavbarController.$inject=['$filter', '$parse' , '$location', 'lodash', 'AuthService', 'AppsService'];
+
 angular.module('fakiyaMainApp')
   .controller('NavbarController', NavbarController);
