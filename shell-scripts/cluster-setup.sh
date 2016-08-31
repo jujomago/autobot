eb7673160c0ac45f7df1e3477aeb6c40eb64fa08
Enrique <enrique@ortuno.net>
2016-08-31 11:38:32 -0400

    Adding volumes and persistence to mongodb

---
+++ shell-scripts/cluster-setup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# Initial cluster setup
+
+#Create mongodb data folders locally
+echo "Creating mongodb data folder locally"
+mkdir -p /var/abx/mongodb
+
+echo "Setup finished."
