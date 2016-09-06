1967417d01f078954d9be5f9b66afe5e0228380c
Enrique <enrique@ortuno.net>
2016-09-06 09:43:12 -0400

    change except validations

--- client/app/features/al/lists/edit/step2-mapping/step2-mapping.controller.js
+++ client/app/features/al/lists/edit/step2-mapping/step2-mapping.controller.js
@@ -379,16 +379,15 @@
         nextStep(){
             console.log('next Step');
             console.log(this.contactFields);
-            let keysCount = _.filter(this.contactFields,{'isKey': true}).length;
-            if(keysCount>0 && keysCount<13){
-                let dataToSend = {
-                        fields: this.contactFields
-                };
-
+            let keysFields = _.filter(this.contactFields,{'isKey': true});
+            if(keysFields.length>0 && keysFields.length<13){
+                let dataToSend = {};
                 if (_$stateParams.settings.listDeleteSettings) {
                     dataToSend.listDeleteSettings = _$stateParams.settings.listDeleteSettings;
+                    dataToSend.fields = keysFields;
                 } else {
                     dataToSend.listUpdateSettings = _$stateParams.settings.listUpdateSettings;
+                    dataToSend.fields = this.contactFields;
                 }
 
                 _$state.go('ap.al.listsEdit-list', {settings:dataToSend, name: _$stateParams.name, manual: true});  
@@ -409,7 +408,9 @@
 
             let fieldsKeys = _.filter(this.contactFields,{'isKey':true});
             let checkSelectedKeys = _checkSelectedFieldKeys(this.hasHeader, this.contactFields, _);
-           
+            if (_$stateParams.settings.listDeleteSettings) {
+                this.contactFields = fieldsKeys;
+            }
             if(fieldsKeys.length>0){
                 if (checkSelectedKeys.length === 0) {
                     let keyNames = _.chain(this.contactFields)
