0b39b0f092caa3c34a715e1dfae6e42622ee64f5
victorm <victor@autoboxcorp.com>
2016-09-07 13:25:11 -0700

    PRs observations

--- client/app/site/apps/apps.controller.js
+++ client/app/site/apps/apps.controller.js
@@ -1,16 +1,10 @@
 'use strict';
 (function(){
-	let _lodash;
-	let _$parse;
 	class AppsComponent {
-	constructor($state, AppsService, lodash, $parse) {
+	constructor($state, AppsService) {
 		this.partners = [];
-		this.apps = [];
 		this.message = {show: false};
 		this.AppsService = AppsService;
-		_$parse = $parse;
-		_lodash = lodash;
-		this.getter = 'partner.partnerName';
 	}
 	$onInit(){
 		this.getApps();
@@ -19,14 +13,7 @@
 	getApps(){
 		return this.AppsService.getApps()
 		.then(response => {
-			this.apps=this.groupBy(response.data);
-			var j = 0;
-			for(let partnerName in this.apps)
-			{
-				console.log(partnerName);
-				this.partners[j] = {apps: this.apps[partnerName], partner: partnerName};
-				j++;
-			}
+			this.partners = response.data;
 			return response;
 		})
 		.catch(error => {
@@ -35,14 +22,8 @@
             return error;
 		});
 	}
-	groupBy(list){
-    let getter = _$parse(this.getter);
-    return _lodash.groupBy(list, function(item) {
-        return getter(item);
-    });
-  }
 }
-	AppsComponent.$inject = ['$state', 'AppsService', 'lodash', '$parse'];
+	AppsComponent.$inject = ['$state', 'AppsService'];
 	angular.module('fakiyaMainApp')
 	  .component('apps', {
 	    templateUrl: 'app/site/apps/apps.html',
