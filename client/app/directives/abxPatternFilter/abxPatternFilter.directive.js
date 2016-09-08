212c6989ff34c462ea933ce6506d311123292ebd
JoelManx <joelmanx2012@gmail.com>
2016-09-08 09:45:44 -0400

    corrections

--- client/app/directives/abxPatternFilter/abxPatternFilter.directive.js
+++ client/app/directives/abxPatternFilter/abxPatternFilter.directive.js
@@ -18,8 +18,8 @@ angular.module('fakiyaMainApp')
         filter:'@abxPatternFilter',
       },
       link: function (scope) {
-       var lastValid = '';
-       var filter = new RegExp(scope.filter.substr(1,scope.filter.length -2));
+       let lastValid = '';
+       let filter = new RegExp(scope.filter.substr(1,scope.filter.length -2));
         scope.$watch('ngModel', function(newValue,oldValue) {
             if(scope.ngModel && !filter.test(newValue)) {
               if(!filter.test(oldValue)){
