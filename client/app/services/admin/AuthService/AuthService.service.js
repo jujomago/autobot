ac1b95bf87f1307efb5a96ff07cd32466f02d1e8
jujomago <jujomago@gmail.com>
2016-09-24 22:22:49 -0400

    improvement token handling

--- client/app/services/admin/AuthService/AuthService.service.js
+++ client/app/services/admin/AuthService/AuthService.service.js
@@ -1,31 +1,47 @@
 'use strict';
 (() => {
-    let _$http, _$cookies, _authManager,_HandleError;
+    let _$http, _$cookies, _authManager,_jwtHelper,_HandleError;
 
-  
+    function _setCookieToken(token,type){
+        console.log('setting cookie');
+        if(type==='persistent'){
+            let tokenDate = _jwtHelper.getTokenExpirationDate(token);   
+            let expDate=new Date(tokenDate);           
+            console.log(`token expDate ${expDate}`);                
+            _$cookies.put('auth_token',token,{'expires': expDate});
+        }else{
+            _$cookies.put('auth_token',token);
+        }             
+    }
     class AuthService {
 
-        constructor($cookies, $http, appConfig, authManager,HandleError) {
+        constructor($cookies, $http, appConfig, authManager,jwtHelper,HandleError) {
             _$http = $http;
             _$cookies = $cookies;          
             _authManager = authManager;
+            _jwtHelper=jwtHelper;
             _HandleError = HandleError;
             if (appConfig.apiUri) {
                 this.endPointUrl = appConfig.apiUri;
             }
         }
         
-        isAuthenticated(){
-             return(_$cookies.get('auth_token'))?true:false;
+        isAuthenticated(){            
+            if(_$cookies.get('auth_token') && !_jwtHelper.isTokenExpired(_$cookies.get('auth_token'))){
+                return true;
+            }
+            return false;
         }
 
         login(credentials) {
             let result = { data: null, statusCode: 200, errorMessage: null };
             return _$http.post(this.endPointUrl + '/auth/login', credentials)
                 .then(response => {
-                    if (response.status === 200) {                         
-                        if (_$cookies.get('auth_token') === undefined) {                       
-                            _$cookies.put('auth_token', response.data);
+                    if (response.status === 200) {
+                         console.log('login del service');
+                        console.log(_$cookies.get('auth_token'));                         
+                        if (!_$cookies.get('auth_token')) {   
+                            _setCookieToken(response.data,'session');                         
                             _authManager.authenticate();
                         }
                         return response;
@@ -38,7 +54,7 @@
             return _$http.put(this.endPointUrl + '/auth/refresh-token', { token: oldToken })
                 .then(response => {
                     if (response.status === 200) {
-                        _$cookies.put('auth_token', response.data);
+                         _setCookieToken(response.data,'session');   
                         _authManager.authenticate();            
                     }
                     return response;                  
@@ -59,7 +75,7 @@
             let result = { data: null, statusCode: 200, errorMessage: null };
             return _$http.get(this.endPointUrl + '/auth/logout')
                 .then(response => {
-                    console.log('cleaning cookie');
+                    console.log('removing cookie');
                     _$cookies.remove('auth_token');
                     _authManager.unauthenticate();
                     return response;
@@ -68,7 +84,7 @@
         }
     }
 
-    AuthService.$inject = ['$cookies', '$http', 'appConfig', 'authManager','HandleError'];
+    AuthService.$inject = ['$cookies', '$http', 'appConfig', 'authManager', 'jwtHelper','HandleError'];
 
     angular.module('fakiyaMainApp')
         .service('AuthService', AuthService);
