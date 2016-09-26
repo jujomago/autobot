ca7d4f5c4c6409fc3fe7b145d86e537c8b2d1ff3
Nelson Araoz <nelson@autoboxcorp.com>
2016-09-26 14:14:29 +0000

    Removing cache for get requests due IE issues
     - config
    
    Related work items: #1683, #1682, #1684

--- client/app/app.js
+++ client/app/app.js
@@ -11,9 +11,20 @@ angular.module('fakiyaMainApp', [
   'ngMessages',
   'angular-jwt'
 ])
-  .config(function ($urlRouterProvider, $locationProvider) {
+  .config(function ($urlRouterProvider, $locationProvider, $httpProvider) {
     $urlRouterProvider.otherwise('/');
     $locationProvider.html5Mode(true);
+    /*This change with httpProvider is for
+    bug 1682: Admin Console: access flow:  The Admin console is displayed when the user does not logged in.
+    bug 1683: Admin Console: access flow:  The form for create is being displaying.
+    bug 1684: Admin Console: access flow:  The submenu is being displaying.
+    */
+    $httpProvider.defaults.cache = false;
+    if (!$httpProvider.defaults.headers.get) {
+      $httpProvider.defaults.headers.get = {};
+    }
+    // disable IE ajax request caching
+    $httpProvider.defaults.headers.get['If-Modified-Since'] = '0';
   }).run(function (lodash, $window, Global) { // jshint ignore:line
     //this method is only for run the lodash deletion
     //TODO: Remove or Evaluate this block after Event Bus is implemented
