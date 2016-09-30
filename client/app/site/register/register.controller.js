70c3dd4be964ba88a0f5ffd7960ce581d2bfb979
victorm <victor@autoboxcorp.com>
2016-09-30 06:54:04 -0700

    register html, controller, api

---
+++ client/app/site/register/register.controller.js
@@ -0,0 +1,64 @@
+'use strict';
+
+(function () {
+
+  let _auth,_Base64Manager, _$location, _$stateParams;
+  class RegisterController {
+
+    constructor($location, $stateParams, AuthService, Base64Manager) {
+      this.company = "";
+      this.email = "";
+      this.firstname = "";
+      this.lastname = "";
+      this.companies = [];
+      this.message = { show: false };
+      this.required = true;
+      _auth = AuthService;
+      _$location = $location;
+      _$stateParams = $stateParams;
+      _Base64Manager = Base64Manager;
+    }
+    $onInit(){
+	  	this.getCompanies();
+	  }
+    getCompanies() {
+      return _auth.getCompanies()
+      .then(response => {
+        this.companies = response.data;
+        console.log(this.companies);
+        return response;
+      })
+      .catch(e => {
+        this.message = { show: true, text: e.errorMessage || e, type: 'danger' };
+        return e;
+      });
+    }
+    register() {
+      let reg = {
+        'email': this.email,
+        'company': this.company,
+        'firstname': this.firstname,
+        'lastname': this.lastname
+      };
+      this.required = true;
+      return _auth.register(reg)
+      .then(response => {
+        this.message = { show: false }
+        return response;
+      })
+      .catch(e => {
+        this.message = { show: true, text: e.errorMessage || e, type: 'danger' };
+        return e;
+      });
+    }
+  }
+
+  RegisterController.$inject = ['$location', '$stateParams', 'AuthService', 'Base64Manager'];
+
+  angular.module('fakiyaMainApp')
+    .component('register', {
+      templateUrl: 'app/site/register/register.html',
+      controller: RegisterController
+    });
+
+})();
---
+++ client/app/site/register/register.controller.spec.js
@@ -0,0 +1,132 @@
+'use strict';
+
+describe('Component: RegisterController', function() {
+
+  // load the controller's module
+  beforeEach(module('fakiyaMainApp'));
+  beforeEach(module('stateMock'));
+
+  var scope;
+  var loginComponent;
+  var state;
+  var _mockStateParams, _mockLocation;
+  var _$cookies;
+  var httpBackend;
+  var endPointUrl;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function(
+    _$httpBackend_,
+    $http,
+    $componentController,
+    $rootScope,
+    $state,
+    $cookies,
+    appConfig) {
+      httpBackend = _$httpBackend_;
+      _mockStateParams = {url: 'L2FwL2FsL2xpc3Rz'};
+      _mockLocation = {
+        url: function(url){
+          if(!url){
+            return this.url;
+          }
+          else{
+            this.url = url;
+            return {search: function(){}};
+          }
+        }
+      };
+      _$cookies = $cookies;
+      scope = $rootScope.$new();
+      state = $state;
+      loginComponent = $componentController('login', {
+        $http: $http,
+        $scope: scope,
+        $stateParams: _mockStateParams,
+        $location: _mockLocation
+      });
+
+      if(appConfig.apiUri){
+          endPointUrl=appConfig.apiUri;
+      }
+
+
+      httpBackend.whenGET(url=>(url.indexOf('.html') !== -1)).respond(200);
+
+  }));
+
+
+  afterEach(function () {
+     httpBackend.verifyNoOutstandingRequest();
+  });
+
+
+  describe('#controllerlogin',()=>{
+      it('=> User logged in Successfully with credentials and redirected to /ap/al/lists',()=>{
+
+
+          loginComponent.username='admin@autoboxcorp.com';
+          loginComponent.password='Password1';
+
+          httpBackend.whenPOST(endPointUrl+'/auth/login',{
+            'username': loginComponent.username,
+            'password': loginComponent.password
+          }).respond('2032820asdfka0s0293ma002');
+
+          loginComponent.login()
+          .then(response=>{
+              expect(response.status).to.equal(200);
+              expect(_$cookies.get('auth_token')).to.equal('2032820asdfka0s0293ma002');
+              expect(_mockLocation.url).to.equal('/ap/al/lists');
+          });
+
+          httpBackend.flush();
+      });
+
+      it('=> User logged in Successfully with credentials and redirected to default page (underconstruction) empty url param',()=>{
+
+          _mockStateParams.url = null;
+          loginComponent.username='admin@autoboxcorp.com';
+          loginComponent.password='Password1';
+
+          httpBackend.whenPOST(endPointUrl+'/auth/login',{
+            'username': loginComponent.username,
+            'password': loginComponent.password
+          }).respond('2032820asdfka0s0293ma002');
+
+
+
+          loginComponent.login()
+          .then(response=>{
+              expect(response.status).to.equal(200);
+              expect(_$cookies.get('auth_token')).to.equal('2032820asdfka0s0293ma002');
+              expect(_mockLocation.url).to.equal('/underconstruction');
+          });
+
+          httpBackend.flush();
+      });
+      it('=> User logged in Successfully with credentials and redirected to default page (underconstruction) corrupted URL',()=>{
+
+          _mockStateParams.url = 'CORRUPTED_URL';
+          loginComponent.username='admin@autoboxcorp.com';
+          loginComponent.password='Password1';
+
+          httpBackend.whenPOST(endPointUrl+'/auth/login',{
+            'username': loginComponent.username,
+            'password': loginComponent.password
+          }).respond('2032820asdfka0s0293ma002');
+
+
+          loginComponent.login()
+          .then(response=>{
+              expect(response.status).to.equal(200);
+              expect(_$cookies.get('auth_token')).to.equal('2032820asdfka0s0293ma002');
+              expect(_mockLocation.url).to.equal('/underconstruction');
+          });
+
+          httpBackend.flush();
+      });
+
+  });
+
+});
