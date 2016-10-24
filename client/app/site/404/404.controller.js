5a120db566a7e7cd46d78610817d10ecae4fb64d
victorm <victor@autoboxcorp.com>
2016-10-24 08:50:37 -0700

    PR comments

--- client/app/site/404/404.controller.js
+++ client/app/site/404/404.controller.js
@@ -2,12 +2,11 @@
 (function(){
 let AuthService;
 class notfoundController {
-  constructor($injector) {
-    AuthService=$injector.get('AuthService');
+  constructor(AuthService) {
     this.isLoggedIn = AuthService.isAuthenticated();
   }
 }
-notfoundController.$inject = ['$injector'];
+notfoundController.$inject = ['AuthService'];
 angular.module('fakiyaMainApp')
   .component('notfound', {
     templateUrl: 'app/site/404/404.html',
