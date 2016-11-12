aa30a892abad2bc7c5ee91b47849193633865fe2
jujomago <jujomago@gmail.com>
2016-11-11 21:08:23 -0400

    form functinoal

--- client/app/services/admin/ContactService/ContactService.service.js
+++ client/app/services/admin/ContactService/ContactService.service.js
@@ -1,31 +1,29 @@
 'use strict';
 (() => {
-    let _$http,_HandleError,_$location;
-
+    let _$http,_HandleError;
     class ContactService {
 
-        constructor($http, appConfig, HandleError, $location) {
-            _$http = $http;           
-            _HandleError = HandleError;
-            _$location = $location;
+        constructor($http, appConfig, HandleError) {
+            _$http = $http;
+            _HandleError = HandleError;           
+         
             if (appConfig.apiUri) {
                 this.endPointUrl = appConfig.apiUri;
             }
         }
 
-        getCompanies() {
+        sendmail(data) {      
             let result = { data: null, statusCode: 200, errorMessage: null };
-            return _$http.get(this.endPointUrl + '/admin/companies')
-                .then(response => {
-                    if (response.status === 200) {
-                      return response.data;
-                    }
-                })
-                .catch(err => _HandleError(err, result));
-        }  
+            return _$http.post(this.endPointUrl + '/admin/services/contact',data)
+            .then(response => {
+              result.data = response.data;
+              return result;
+            })
+            .catch(err => _HandleError(err, result));       
+        }    
     }
 
-    ContactService.$inject = ['$http', 'appConfig', 'HandleError', '$location'];
+    ContactService.$inject = ['$http', 'appConfig', 'HandleError'];
     angular.module('fakiyaMainApp')
         .service('ContactService', ContactService);
 })();
