a472c8ba09cd997b3ca2bfe09e4a64b195df08f1
jujomago <jujomago@gmail.com>
2016-11-24 11:06:45 -0400

    more advances

--- client/app/directives/abxSidebar/abxSidebar.directive.js
+++ client/app/directives/abxSidebar/abxSidebar.directive.js
@@ -1,17 +1,36 @@
 'use strict';
 
 angular.module('fakiyaMainApp')
-  .directive('abxSidebar', ['$location', function($location) {
+  .directive('abxSidebar', ['$location', '$window', function ($location, $window) {
 
-  function link(scope) {
-    scope.checkActive = function(link){
-      		return ($location.path().indexOf(link) >= 0) ? 'active' : '';
+    function link(scope, element) {
+      scope.checkActive = function (link) {
+        return ($location.path().indexOf(link) >= 0) ? 'active' : '';
+      };
+      
+      let navbarHeight = 60;
+      let footerHeight = 50;
+      let minHeightSideBar;
+
+      function calcMinHeight(innerHeight){
+        console.log('calling minheight');
+         minHeightSideBar = innerHeight - navbarHeight - footerHeight;
+         element.css('min-height', minHeightSideBar);
+      }
+      
+      calcMinHeight($window.innerHeight);
+      angular.element($window).on('resize', function() {
+            scope.$apply(function() {
+                calcMinHeight($window.innerHeight);
+            });
+      });
+
+     
+    }
+
+    return {
+      templateUrl: 'app/directives/abxSidebar/abxSidebar.html',
+      restrict: 'EA',
+      link: link
     };
-  }
-
-  return {
-    templateUrl: 'app/directives/abxSidebar/abxSidebar.html',
-    restrict: 'EA',
-    link: link
-  };
-}]);
+  }]);
