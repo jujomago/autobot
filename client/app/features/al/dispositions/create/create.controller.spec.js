55174138e8f2450ed4603c104d25b86a242578c7
Enrique <enrique@ortuno.net>
2017-01-04 12:00:01 -0400

    change tests

--- client/app/features/al/dispositions/create/create.controller.spec.js
+++ client/app/features/al/dispositions/create/create.controller.spec.js
@@ -28,7 +28,7 @@ describe('Component:DispositionCreateComponent', function () {
     });
 
     httpBackend.whenGET(url => (url.indexOf('.html') !== -1)).respond(200);
-    httpBackend.whenGET(appConfig.apiUri+'/f9/admin/lists/%$&unexisting_list)(*&^%^').respond(200);
+    httpBackend.whenPOST(appConfig.apiUri+'/f9/admin/lists').respond(200);
 
   }));
   describe('#timeField', () => {
